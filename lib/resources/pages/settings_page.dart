e03274aa3a1d70b843a0e16328d75f8e92cb8b18
mee <baramee1407@gmail.com>
2022-06-06 23:57:13 +0700

    First Commit

---
+++ lib/resources/pages/settings_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class SettingsPage extends StatefulWidget {
+  SettingsPage({Key? key}) : super(key: key);
+  
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends NyState<SettingsPage> {
+
+  @override
+  init() async {
+    
+  }
+  
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: SafeArea(
+         child: Container(
+           child: Text('Settings'),
+         ),
+      ),
+    );
+  }
+}
